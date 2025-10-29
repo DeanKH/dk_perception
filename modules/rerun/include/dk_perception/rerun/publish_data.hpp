9545d1780ac90195b5e6c891c6da8a4f5e6cf639
deankh <12647494+DeanKH@users.noreply.github.com>
2025-10-30 02:08:25 +0900

    rerunモジュールを追加

--- modules/rerun/include/dk_perception/rerun/publish_data.hpp
+++ modules/rerun/include/dk_perception/rerun/publish_data.hpp
@@ -3,6 +3,16 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 // #include <gtsam_app/geoms.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <voxblox/core/color.h>
+#include <voxblox/core/common.h>
+#include <voxblox/core/layer.h>
+#include <voxblox/core/voxel.h>
+
+#include <Eigen/Geometry>
+#include <algorithm>
+#include <cassert>
 #include <dk_perception/geometry.hpp>
 #include <rerun.hpp>
 #include <rerun/archetypes/boxes3d.hpp>
@@ -11,46 +21,78 @@
 #include <rerun/recording_stream.hpp>
 #include <vector>
 
-namespace dklib::perception::visualization {
-/**
- * Publish a 2D bounding box to Rerun.
- * 高さ0, Yaw軸以外の回転なしで2Dの矩形をRerunに出力します．
- * 矩形は中心の点と矩形のポリゴンを出力します．
- */
+#include "rerun/archetypes/boxes3d.hpp"
+#include "rerun/archetypes/points3d.hpp"
+#include "rerun/archetypes/transform3d.hpp"
+#include "rerun/rotation3d.hpp"
+
+namespace dklib::perception::publisher {
 std::string publishData(const rerun::RecordingStream& rec, const std::string& entity,
-                        const geometry::BoundingBox2D& bbox, const std::array<uint8_t, 4> color = {0, 255, 0, 255},
-                        const float line_radius = 0.02f) {
-  std::vector<rerun::Vec3D> centers = {{bbox.center.x(), bbox.center.y(), 0.0f}};
-  std::vector<rerun::Vec3D> sizes = {{bbox.size.x(), bbox.size.y(), 0.00f}};
-
-  Eigen::Quaternionf q;
-  q = Eigen::AngleAxisf(bbox.rotation, Eigen::Vector3f::UnitZ());
-  std::vector<rerun::Quaternion> orientations = {rerun::Quaternion::from_wxyz(q.w(), q.x(), q.y(), q.z())};
-
-  std::vector<rerun::Rgba32> colors = {rerun::Rgba32{color[0], color[1], color[2], color[3]}};
-  auto boxes = rerun::Boxes3D::from_centers_and_sizes(centers, sizes)
-                   .with_quaternions(orientations)
-                   .with_colors(colors)
-                   .with_radii({line_radius});
-  std::string entity_path = entity + "/bbox";
-  rec.log(entity_path, boxes);
-  return entity_path;
-}
+                        const Eigen::Isometry3d& transform);
 
 std::string publishData(const rerun::RecordingStream& rec, const std::string& entity,
-                        const std::vector<Eigen::Vector2f>& points,
-                        const std::array<uint8_t, 4>& color = {0, 0, 255, 255}, const float radius = 0.02f) {
+                        const dklib::perception::geometry::BoundingBox3D& bbox,
+                        const std::array<uint8_t, 4> color = {0, 255, 0, 255}, const float line_radius = 0.005f);
+
+template <typename PointT>
+std::string publishData(const rerun::RecordingStream& rec, const std::string& entity,
+                        typename pcl::PointCloud<PointT>::Ptr& cloud, const float radius = 0.005f) {
   std::vector<rerun::Vec3D> pts;
-  pts.reserve(points.size());
-  for (const auto& p : points) {
-    pts.emplace_back(p.x(), p.y(), 0.0f);
+  pts.reserve(cloud->size());
+  for (const auto& p : cloud->points) {
+    pts.emplace_back(p.x, p.y, p.z);
   }
-  auto point_cloud =
-      rerun::Points3D(pts)
-          .with_colors(std::vector<rerun::Rgba32>(pts.size(), rerun::Rgba32{color[0], color[1], color[2], color[3]}))
-          .with_radii({radius});
+  std::vector<rerun::Rgba32> colors;
+  colors.reserve(cloud->size());
+  for (const auto& p : cloud->points) {
+    colors.emplace_back(p.r, p.g, p.b, 255);
+  }
+  auto point_cloud = rerun::Points3D(pts).with_colors(colors).with_radii({radius});
   std::string entity_path = entity + "/points";
   rec.log(entity_path, point_cloud);
   return entity_path;
 }
-}  // namespace dklib::perception::visualization
+
+template <>
+std::string publishData<pcl::PointXYZI>(const rerun::RecordingStream& rec, const std::string& entity,
+                                        typename pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud, const float radius);
+
+template <typename PointT>
+std::string publishVoxelData(const rerun::RecordingStream& rec, const std::string& entity,
+                             typename pcl::PointCloud<PointT>::Ptr& cloud, const float voxel_size) {
+  rerun::Boxes3D boxes;
+  std::vector<rerun::Vec3D> centers;
+  std::vector<rerun::Vec3D> sizes;
+  centers.reserve(cloud->size());
+  sizes.reserve(cloud->size());
+  for (const auto& p : cloud->points) {
+    rerun::Vec3D center{p.x, p.y, p.z};
+    rerun::Vec3D size{voxel_size * 0.5f, voxel_size * 0.5f, voxel_size * 0.5f};
+    centers.push_back(center);
+    sizes.push_back(size);
+  }
+  std::vector<rerun::Rgba32> colors;
+  colors.reserve(cloud->size());
+
+  const float intensity_max =
+      std::max_element(cloud->points.begin(), cloud->points.end(), [](const auto& a, const auto& b) {
+        return a.intensity < b.intensity;
+      })->intensity;
+
+  for (const auto& p : cloud->points) {
+    if (p.intensity < 0) {
+      colors.emplace_back(0, 0, 255, 255);
+    } else {
+      double diff = intensity_max - p.intensity;
+      auto c = voxblox::rainbowColorMap(diff / intensity_max);
+      colors.emplace_back(c.r, c.g, c.b, 100);
+    }
+  }
+
+  rerun::Boxes3D voxel_boxes = rerun::Boxes3D::from_centers_and_half_sizes(centers, sizes)
+                                   .with_colors(colors)
+                                   .with_fill_mode(rerun::FillMode::Solid);
+  rec.log(entity, voxel_boxes);
+  return entity;
+}
+}  // namespace dklib::perception::publisher
--- modules/rerun/src/publish_data.cpp
+++ modules/rerun/src/publish_data.cpp
@@ -1,1 +1,74 @@
 #include "dk_perception/rerun/publish_data.hpp"
+
+namespace dklib::perception::publisher {
+std::string publishData(const rerun::RecordingStream& rec, const std::string& entity,
+                        const Eigen::Isometry3d& transform) {
+  auto translation = rerun::Vec3D{transform.translation().cast<float>().x(), transform.translation().cast<float>().y(),
+                                  transform.translation().cast<float>().z()};
+  Eigen::Quaterniond q;
+  q = transform.rotation();
+  auto orientation = rerun::Quaternion::from_wxyz(q.w(), q.x(), q.y(), q.z());
+  auto rotation = rerun::Rotation3D(orientation);
+  auto tf = rerun::Transform3D::from_translation_rotation(translation, rotation);
+  rec.log(entity, tf);
+
+  return entity;
+}
+
+std::string publishData(const rerun::RecordingStream& rec, const std::string& entity,
+                        const dklib::perception::geometry::BoundingBox3D& bbox, const std::array<uint8_t, 4> color,
+                        const float line_radius) {
+  std::vector<rerun::Vec3D> centers = {
+      {bbox.center.cast<float>().x(), bbox.center.cast<float>().y(), bbox.center.cast<float>().z()}};
+  std::vector<rerun::Vec3D> sizes = {
+      {bbox.size.cast<float>().x(), bbox.size.cast<float>().y(), bbox.size.cast<float>().z()}};
+
+  Eigen::Quaternionf q = bbox.orientation.cast<float>();
+  std::vector<rerun::Quaternion> orientations = {rerun::Quaternion::from_wxyz(q.w(), q.x(), q.y(), q.z())};
+
+  std::vector<rerun::Rgba32> colors = {rerun::Rgba32{color[0], color[1], color[2], color[3]}};
+  auto boxes = rerun::Boxes3D::from_centers_and_sizes(centers, sizes)
+                   .with_quaternions(orientations)
+                   .with_colors(colors)
+                   .with_radii({line_radius});
+  std::string entity_path = entity + "/bbox";
+  rec.log(entity_path, boxes);
+  return entity_path;
+}
+
+template <>
+std::string publishData<pcl::PointXYZI>(const rerun::RecordingStream& rec, const std::string& entity,
+                                        typename pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud, const float radius) {
+  std::vector<rerun::Vec3D> pts;
+  std::vector<rerun::Rgba32> colors;
+  pts.reserve(cloud->size());
+  colors.reserve(cloud->size());
+
+  const float intensity_max =
+      std::max_element(cloud->points.begin(), cloud->points.end(), [](const auto& a, const auto& b) {
+        return a.intensity < b.intensity;
+      })->intensity;
+  const float intensity_min =
+      std::min_element(cloud->points.begin(), cloud->points.end(), [](const auto& a, const auto& b) {
+        return a.intensity < b.intensity;
+      })->intensity;
+  std::cout << "Intensity min/max: " << intensity_min << " / " << intensity_max << std::endl;
+
+  for (const auto& p : cloud->points) {
+    pts.emplace_back(p.x, p.y, p.z);
+    if (p.intensity > 0) {
+      auto a = intensity_max - p.intensity;
+      colors.emplace_back(255 * a / intensity_max, 0, 0, 255);
+    } else {
+      auto a = p.intensity - intensity_min;
+      colors.emplace_back(0, 255 * a / std::fabs(intensity_min), 0, 255);
+    }
+  }
+
+  auto point_cloud = rerun::Points3D(pts).with_colors(colors).with_radii({radius});
+  std::string entity_path = entity + "/points";
+  rec.log(entity_path, point_cloud);
+  return entity_path;
+}
+
+}  // namespace dklib::perception::publisher
